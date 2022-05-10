6d9fb1c7b32d5fba6f1f958f601570ab6feec987
gabinha22 <99849719+gabinha22@users.noreply.github.com>
2022-05-10 19:36:38 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,50 @@
+const chart = document.querySelector("#chart").getContext('2d');
+
+new Chart(chart, {
+    type: 'line',
+    data:{
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+
+        datasets: [
+            {
+                label: 'BTC',
+                data: [29374, 33537, 49631, 59095, 57828, 36684, 33572, 39974, 48847, 48116, 61004],
+                borderColor: 'red',
+                borderWidth: 2
+            },
+            {
+                label: 'DOG',
+                data: [31500, 41000, 88800, 26000, 46000, 32698, 5000, 3000, 18656, 24832, 36844],
+                borderColor: 'blue',
+                borderWidth: 2
+            }
+        ]
+        
+    },
+    options: {
+        responsive: true
+    }
+})
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sideBar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+    sideBar.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideBar.style.display = 'none';
+})
+
+
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+    
+})
